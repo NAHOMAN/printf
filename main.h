4719fcee2a209b692569c19a519f83854d408a38
2NG CODER <nahomnigussie7@gmail.com>
2023-01-27 02:35:01 -0800

    printf group task

--- main.h
+++ main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -97,4 +98,5 @@ int append_hexa_code(char, char[], int);
 int is_digit(char);
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
+
 #endif /* MAIN_H */
